a3e879c65d6a9601719370aa91ca2e3e97d467f2
scriptskali <104281088+scriptskali@users.noreply.github.com>
2022-04-23 20:09:52 -0400

    Update and rename Botnet.py to WHILER.py

---
+++ WHILER.py
@@ -0,0 +1,22 @@
+import requests
+import os
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[39m'
+os.system("clear")
+print(GREEN+"░█▀▀█ ░█▀▀▀█ ▀▀█▀▀ ── ░█──░█ ░█─░█ ▀█▀ ░█─── ░█▀▀▀")
+print("      ░█▀▀▄ ░█──░█ ─░█── ▀▀ ░█░█░█ ░█▀▀█ ░█─ ░█─── ░█▀▀▀")
+print("      ░█▄▄█ ░█▄▄▄█ ─░█── ── ░█▄▀▄█ ░█─░█ ▄█▄ ░█▄▄█ ░█▄▄▄")
+       
+       username = ""
+       username = input("usuario:")
+       while True:
+             print (("[*]"),"REPORT-BOT FOR TIKTOK USER @", (username), (b))
+             sleep(1)
+             b = b + 1
