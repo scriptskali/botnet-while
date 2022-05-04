2b9219ba1a8bb9c3ab4d320ff9372c52ee1256a2
Arch0zz <104281088+scriptskali@users.noreply.github.com>
2022-05-04 13:24:24 -0400

    Update WHILER.py

--- WHILER.py
+++ WHILER.py
@@ -1,20 +1,4 @@
 import requests
-import os
-BLACK = '\033[30m'
 RED = '\033[31m'
-GREEN = '\033[32m'
-YELLOW = '\033[33m'
-BLUE = '\033[34m'
-MAGENTA = '\033[35m'
-CYAN = '\033[36m'
-WHITE = '\033[37m'
-RESET = '\033[39m'
-os.system("clear")
-print(GREEN+"░█▀▀█ ░█▀▀▀█ ▀▀█▀▀ ── ░█──░█ ░█─░█ ▀█▀ ░█─── ░█▀▀▀")
-print("      ░█▀▀▄ ░█──░█ ─░█── ▀▀ ░█░█░█ ░█▀▀█ ░█─ ░█─── ░█▀▀▀")
-print("      ░█▄▄█ ░█▄▄▄█ ─░█── ── ░█▄▀▄█ ░█─░█ ▄█▄ ░█▄▄█ ░█▄▄▄")
-       username = input("usuario:")
-       while True:
-             print (("[*]"),"REPORT-BOT FOR TIKTOK USER @", (username), (b))
-             sleep(1)
-             b = b + 1
+print(RED+"NO")
+print("DISPONIBLE")
